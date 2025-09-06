5d18778de446e2cef58f743b141add797c10975a
Fernanda Barbieri <ferbarbieri21@gmail.com>
2025-09-06 11:49:12 -0300

    Exercício Mundo 3

--- tuplaaleatorio.py
+++ tuplaaleatorio.py
@@ -1,1 +1,4 @@
 from random import randint
+numeros = tuple(randint(1, 10) for n in range(5))
+print(f'Os números sorteados foram: {numeros}')
+print(f'O maior número foi {max(numeros)} e o menor foi {min(numeros)}')
