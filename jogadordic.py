6e4414cf8b8fa94e423a70784fa4ccab9ef51934
Fernanda Barbieri <ferbarbieri21@gmail.com>
2025-09-24 10:04:05 -0300

    Exercício Mundo 3

--- jogadordic.py
+++ jogadordic.py
@@ -1,16 +1,52 @@
+time = []
 jogador = {}
 
-jogador['Nome'] = input('Nome do Jogador: ')
-jogador['Partidas'] = int(input(f'Quantas partidas {jogador['Nome']} jogou? '))
+while True:
+    jogador.clear()
+    jogador['Nome'] = input('Nome do Jogador: ')
+    total_partidas = int(input(f'Quantas partidas {jogador['Nome']} jogou? '))
 
-gols = []
-for g in range(0, jogador['Partidas']+1):
-    gols.append(int(input(f'Número de gols na {g}° partida: ')))
+    gols = []
+    for g in range(0, total_partidas):
+        gols.append(int(input(f'Número de gols na {g+1}ª partida: ')))
 
-jogador['Gols'] = gols
-jogador['Total de Gols'] = sum(gols)
+    jogador['Gols'] = gols
+    jogador['Total de Gols'] = sum(gols)
+    time.append(jogador.copy())
 
-print(f'O jogador {jogador['Nome']} jogou {jogador['Partidas']} partidas.')
-for i, gols in enumerate(jogador['Gols']): #posso utilizar o enumarate pois jogador['Gols'] é uma lista dentro do dicionário jogador
-    print(f'Na partida {i+1}, fez {gols} gols.')
-print(f'Total de gols: {jogador['Total de Gols']}')
+
+    while True:
+        resp = input('Cadastrar mais jogadores? [S/N] ').strip().upper()
+        if resp in ['S', 'N']:
+            break
+        print('Erro. Responda apenas S ou N.')
+    if resp == 'N':
+        break
+
+print('-=' * 25)
+
+print(f'{"Cod":<4}{"Nome":<15}{"Gols":<20}{"Total":>6}')
+print('-' * 50)
+
+for k, v in enumerate(time):
+    print(f'{k:<4}{v["Nome"]:<15}{str(v["Gols"]):<20}{v["Total de Gols"]:>6}')
+print('-' * 50)
+
+while True:
+    try:
+        busca = int(input('Mostrar dados de qual jogador? (999 para parar) '))
+    except ValueError:
+        print('Erro. Por favor, digite um número válido.')
+        continue
+
+    if busca == 999:
+        print("Encerrando...")
+        break
+    
+    if busca >= len(time) or busca < 0:
+        print(f'Erro. Não existe jogador com o código {busca}.')
+    else:
+        print(f'-- LEVANTAMENTO DO JOGADOR {time[busca]['Nome']}:')
+        for i, g in enumerate(time[busca]['Gols']):
+            print(f'   => Na partida {i+1}, fez {g} gol(s).')
+    print('-' * 50)
